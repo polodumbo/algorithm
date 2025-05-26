db3327b21f849f24e03f9f513fda81ca853f0c0c
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2025-05-26 21:22:19 +0900

    [Bronze I] Title: 소인수분해, Time: 164 ms, Memory: 14748 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -1,14 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(br.readLine());
-        List<Integer> factorList = new ArrayList<>();
 
         for (int i = 2; i <= num; i++) {
             while (true) {
@@ -16,13 +13,9 @@ public class Main {
                     break;
                 } else {
                     num /= i;
-                    factorList.add(i);
+                    System.out.println(i);
                 }
             }
-        }
-
-        for (Integer i : factorList) {
-            System.out.println(i);
         }
 
         br.close();
