fd2a53f67651da9edbd9658b23f7806bcb420138
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2025-05-28 00:00:41 +0900

    [Silver V] Title: 색종이, Time: 172 ms, Memory: 17808 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean[][] whites = new boolean[100][100];
+        int paperCount = sc.nextInt();
+        int area = 0;
+
+        for (int i = 0; i < paperCount; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            for (int j = x; j < x + 10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    if (!whites[j][k]) {
+                        whites[j][k] = true;
+                        area++;
+                    }
+                }
+            }
+        }
+        
+        System.out.println(area);
+    }
+}
