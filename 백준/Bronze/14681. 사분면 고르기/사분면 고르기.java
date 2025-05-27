33917ce300ff087d3001487eb94a1932505e67f3
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2025-05-27 17:19:32 +0900

    [Bronze V] Title: 사분면 고르기, Time: 112 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(br.readLine());
+        int y = Integer.parseInt(br.readLine());
+        int quadrant = 0;
+
+        if (x > 0) {
+            quadrant = y > 0 ? 1 : 4;
+        } else if (x < 0) {
+            quadrant = y > 0 ? 2 : 3;
+        }
+
+        System.out.println(quadrant);
+        br.close();
+    }
+}
