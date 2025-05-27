12f5c0cd2a15952082aeeddf0f1f351afcae47f0
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2025-05-27 16:46:21 +0900

    [Bronze V] Title: 시험 성적, Time: 100 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int score = Integer.parseInt(br.readLine());
+        char grade = ' ';
+
+        if (score >= 90) grade = 'A';
+        else if (score >= 80) grade = 'B';
+        else if (score >= 70) grade = 'C';
+        else if (score >= 60) grade = 'D';
+        else grade = 'F';
+
+        System.out.println(grade);
+    }
+}
