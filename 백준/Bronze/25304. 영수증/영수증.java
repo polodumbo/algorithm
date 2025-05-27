7b3d44f247bb20966825b3779318271e0b144669
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2025-05-27 18:13:21 +0900

    [Bronze IV] Title: 영수증, Time: 112 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+
+        int total = 0;
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
+            total += x * n;
+        }
+
+        System.out.println(X == total ? "Yes" : "No");
+        br.close();
+    }
+}
