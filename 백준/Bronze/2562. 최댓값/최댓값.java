5a924f96a173adace882ac78d021d45dbdc07ea1
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2025-05-28 13:27:40 +0900

    [Bronze III] Title: 최댓값, Time: 120 ms, Memory: 16044 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,21 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int max = 0, index = 0;
+        List<Integer> numList = new ArrayList<>();
 
         for (int i = 0; i < 9; i++) {
-            int n = Integer.parseInt(br.readLine());
-
-            if (n > max) {
-                max = n;
-                index = i + 1;
-            }
+            numList.add(Integer.parseInt(br.readLine()));
         }
+
+        int max = Collections.max(numList);
+        int index = numList.indexOf(max) + 1;
 
         System.out.println(max + "\n" + index);
         br.close();
