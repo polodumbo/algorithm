5c003f9acd56abf15479c8fcd5691f50500871a3
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2025-05-29 23:00:32 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 100 ms, Memory: 14040 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,25 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        List<Integer> students = new ArrayList<>();
-        
-        for (int i = 1; i <= 30; i++) {
-            students.add(i);
-        }
+        boolean[] students = new boolean[30];
 
         for (int i = 0; i < 28; i++) {
-            students.remove(students.indexOf(Integer.parseInt(br.readLine())));
+            students[Integer.parseInt(br.readLine()) - 1] = true;
         }
 
-        sb.append(Collections.min(students)).append("\n").append(Collections.max(students));
+        for (int i = 0; i < 30; i++) {
+            if (!students[i]) {
+                sb.append(i + 1).append("\n");
+            }
+        }
 
         System.out.println(sb);
         br.close();
