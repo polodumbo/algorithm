843020c6ede54b84569a1b48fa53934f5e1ca71f
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2025-05-29 22:25:14 +0900

    [Bronze II] Title: 공 바꾸기, Time: 108 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] balls = new int[N];
+
+        for (int i = 0; i < balls.length; i++) {
+            balls[i] = i + 1;
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int I = Integer.parseInt(st.nextToken());
+            int J = Integer.parseInt(st.nextToken());
+            int temp = balls[I - 1];
+
+            balls[I - 1] = balls[J - 1];
+            balls[J - 1] = temp;
+        }
+
+        for (int b : balls) {
+            sb.append(b).append(" ");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
