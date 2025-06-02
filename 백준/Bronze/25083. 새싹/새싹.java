6400ee853c71c9e97df0a8904bfc56bf03807975
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2025-06-02 16:20:36 +0900

    [Bronze V] Title: 새싹, Time: 96 ms, Memory: 14020 KB -BaekjoonHub

--- 백준/Bronze/25083. 새싹/새싹.java
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -2,13 +2,11 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        sb.append("         ,r'\"7\n" +
+        System.out.println("         ,r'\"7\n" +
                 "r`-_   ,'  ,/\n" +
                 " \\. \". L_r'\n" +
                 "   `~\\/\n" +
                 "      |\n" +
                 "      |");
-        System.out.println(sb);
     }
 }
