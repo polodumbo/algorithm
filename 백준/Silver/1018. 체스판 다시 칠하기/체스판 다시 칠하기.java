f81f641607c488abab77bd9e73d0ee1bc1298b8d
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2025-06-09 16:19:31 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        Character[][] board = new Character[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < M; j++) {
+                board[i][j] = str.charAt(j);
+            }
+        }
+
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < N - 7; i++) {
+            for (int j = 0; j < M - 7; j++) {
+                int count = 0;
+                char f = board[i][j];
+                for (int k = i; k < i + 8; k++) {
+                    for (int l = j; l < j + 8; l++) {
+                        if (board[k][l] != f) {
+                            count++;
+                        }
+                        f = f == 'W' ? 'B' : 'W';
+                    }
+                    f = f == 'W' ? 'B' : 'W';
+                }
+                min = Math.min(min, Math.min(count, 64 - count));
+            }
+        }
+
+        System.out.println(min);
+        br.close();
+    }
+}
