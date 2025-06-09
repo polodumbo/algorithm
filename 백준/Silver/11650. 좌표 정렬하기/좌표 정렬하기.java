e47cbb4d942235f756d952b41dd654f671c4dcfa
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2025-06-10 00:08:28 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 664 ms, Memory: 50616 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] xy = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 2; j++) {
+                xy[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        Arrays.sort(xy, (a, b) -> {
+            if (a[0] == b[0]) {
+                return a[1] - b[1];
+            } else {
+                return a[0] - b[0];
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(xy[i][0]).append(" ").append(xy[i][1]).append("\n");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
