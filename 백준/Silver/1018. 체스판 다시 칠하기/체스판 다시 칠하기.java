0a4457bc3fb6465cb65b53d1caae504c322aed34
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2025-06-09 16:22:34 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 116 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -10,12 +10,16 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        Character[][] board = new Character[N][M];
+        boolean[][] board = new boolean[N][M];
 
         for (int i = 0; i < N; i++) {
             String str = br.readLine();
             for (int j = 0; j < M; j++) {
-                board[i][j] = str.charAt(j);
+                if (str.charAt(j) == 'B') {
+                    board[i][j] = false;
+                } else {
+                    board[i][j] = true;
+                }
             }
         }
 
@@ -24,15 +28,15 @@ public class Main {
         for (int i = 0; i < N - 7; i++) {
             for (int j = 0; j < M - 7; j++) {
                 int count = 0;
-                char f = board[i][j];
+                boolean f = board[i][j];
                 for (int k = i; k < i + 8; k++) {
                     for (int l = j; l < j + 8; l++) {
                         if (board[k][l] != f) {
                             count++;
                         }
-                        f = f == 'W' ? 'B' : 'W';
+                        f = !f;
                     }
-                    f = f == 'W' ? 'B' : 'W';
+                    f = !f;
                 }
                 min = Math.min(min, Math.min(count, 64 - count));
             }
