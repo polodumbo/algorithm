b1242d5a535dcb6d8af3025d3e76dddb311dc277
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2025-06-10 00:12:41 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 636 ms, Memory: 51624 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -19,9 +19,9 @@ public class Main {
 
         Arrays.sort(xy, (a, b) -> {
             if (a[0] == b[0]) {
-                return a[1] - b[1];
+                return Integer.compare(a[1], b[1]);
             } else {
-                return a[0] - b[0];
+                return Integer.compare(a[0], b[0]);
             }
         });
 
