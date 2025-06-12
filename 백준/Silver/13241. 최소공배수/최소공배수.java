ff79cebdef0bc902d3e7e9878c6badaa2b1fc780
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2025-06-13 01:21:40 +0900

    [Silver V] Title: 최소공배수, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Integer.parseInt(st.nextToken());
+        long B = Integer.parseInt(st.nextToken());
+
+        System.out.println(A * B / getGCD(Math.max(A, B), Math.min(A, B)));
+        br.close();
+    }
+
+    public static long getGCD(long a, long b) {
+        while (b != 0) {
+            long temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+}
