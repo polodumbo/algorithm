d40d52177b1ec9acd7a2b948351a3b0038623de5
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2025-06-17 03:08:30 +0900

    [Silver IV] Title: 카드2, Time: 164 ms, Memory: 28944 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Queue<Integer> queue = new ArrayDeque<>();
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            queue.offer(i);
+        }
+
+        while (queue.size() > 1) {
+            queue.poll();
+            queue.offer(queue.poll());
+        }
+
+        System.out.println(queue.poll());
+        br.close();
+    }
+}
