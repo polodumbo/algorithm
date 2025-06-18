7d54c4f767f931b6860d160f81e20d570101787c
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2025-06-18 21:55:07 +0900

    [Bronze I] Title: 약수, Time: 104 ms, Memory: 14392 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        List<Integer> list = new ArrayList<>();
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int n = Integer.parseInt(st.nextToken());
+            list.add(n);
+        }
+        Collections.sort(list);
+
+        System.out.println(list.get(0) * list.get(list.size() - 1));
+        br.close();
+    }
+}
