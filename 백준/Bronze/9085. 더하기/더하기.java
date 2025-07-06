7b171aef18850d03435965808c05e9e1a19a5c7a
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2025-07-06 15:17:17 +0900

    [Bronze III] Title: 더하기, Time: 100 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/9085. 더하기/더하기.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            int sum = 0;
+            int N = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                sum += Integer.parseInt(st.nextToken());
+            }
+            sb.append(sum).append('\n');
+        }
+        System.out.println(sb);
+        br.close();
+    }
+}
