d943a6251d00739af37e80bd8996f323ad1af8ff
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2025-07-06 15:15:10 +0900

    [Bronze V] Title: 세금, Time: 124 ms, Memory: 15976 KB -BaekjoonHub

---
+++ 백준/Bronze/20492. 세금/세금.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println((N * 78 / 100) + " " + (N - N * 44 / 1000));
+        br.close();
+    }
+}
