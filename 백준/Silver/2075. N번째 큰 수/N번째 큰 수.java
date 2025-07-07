e4aca38b8f06b62abdd4ac583d9b7819c2cd5be6
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2025-07-07 09:48:02 +0900

    [Silver III] Title: N번째 큰 수, Time: 1908 ms, Memory: 285864 KB -BaekjoonHub

---
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                list.add(Integer.parseInt(st.nextToken()));
+            }
+        }
+        Collections.sort(list);
+        System.out.println(list.get(N * N - N));
+        br.close();
+    }
+}
