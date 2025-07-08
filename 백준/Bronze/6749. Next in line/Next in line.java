a0d5100484d56d6b1f29d7fa8b3a0a4f2d1f0dc0
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2025-07-08 12:58:59 +0900

    [Bronze IV] Title: Next in line, Time: 104 ms, Memory: 14028 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int Y = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        System.out.println(M + M - Y);
+        br.close();
+    }
+}
