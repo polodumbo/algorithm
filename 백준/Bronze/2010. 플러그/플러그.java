5be1f2947ba308e037dd6c6f3fd53074539d7e18
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2025-07-27 23:56:12 +0900

    [Bronze III] Title: 플러그, Time: 228 ms, Memory: 41332 KB -BaekjoonHub

---
+++ 백준/Bronze/2010. 플러그/플러그.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+        for (int i = 0; i < N; i++) {
+            count += Integer.parseInt(br.readLine());
+        }
+        count -= N - 1;
+
+        System.out.println(count);
+        br.close();
+    }
+}
