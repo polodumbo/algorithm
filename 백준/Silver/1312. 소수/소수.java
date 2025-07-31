9a38d232dd535be678e577862b16820bfa647807
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2025-07-31 10:12:50 +0900

    [Silver V] Title: 소수, Time: 116 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Silver/1312. 소수/소수.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        int result = A % B;
+        for (int i = 0; i < N - 1; i++) {
+            result *= 10;
+            result %= B;
+        }
+
+        System.out.println(result * 10 / B);
+        br.close();
+    }
+}
