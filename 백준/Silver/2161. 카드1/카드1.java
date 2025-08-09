4d3e5dbb80f3c8fe54c91ba38a8226504903e3e2
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2025-08-10 00:26:18 +0900

    [Silver V] Title: 카드1, Time: 108 ms, Memory: 14424 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            queue.offer(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (queue.size() > 1) {
+            sb.append(queue.poll()).append(' ');
+            queue.offer(queue.poll());
+        }
+        sb.append(queue.poll());
+
+        System.out.println(sb);
+        br.close();
+    }
+}
