a3b8258dcb741d1e4a1421af9da7c71328c3e382
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2025-11-14 20:26:02 +0900

    [Bronze V] Title: Bottle Return, Time: 100 ms, Memory: 14016 KB -BaekjoonHub

---
+++ 백준/Bronze/21300. Bottle Return/Bottle Return.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int total = 0;
+        for (int i = 0; i < 6; i++) {
+            int n = Integer.parseInt(st.nextToken());
+            total += n;
+        }
+
+        System.out.println(total * 5);
+        br.close();
+    }
+}
