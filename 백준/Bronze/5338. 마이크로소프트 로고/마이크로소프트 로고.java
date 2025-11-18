c87b25c3218b801aa005c71a91871ed30adcd061
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2025-11-18 13:30:23 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 96 ms, Memory: 14000 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("       _.-;;-._\n"
+            + "'-..-'|   ||   |\n"
+            + "'-..-'|_.-;;-._|\n"
+            + "'-..-'|   ||   |\n"
+            + "'-..-'|_.-''-._|");
+    }
+}
