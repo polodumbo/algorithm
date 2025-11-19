c66a09617e87b6ca51646a8be9b0e554a40e71e4
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2025-11-19 09:16:04 +0900

    [Bronze IV] Title: 특식 배부, Time: 100 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/27110. 특식 배부/특식 배부.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        int result = 0;
+        result += Math.min(N, A);
+        result += Math.min(N, B);
+        result += Math.min(N, C);
+
+        System.out.println(result);
+        br.close();
+    }
+}
