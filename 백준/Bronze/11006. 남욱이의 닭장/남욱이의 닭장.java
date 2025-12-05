4e70371edc4675eb7d246fd401967516417df89e
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2025-12-05 11:53:33 +0900

    [Bronze III] Title: 남욱이의 닭장, Time: 96 ms, Memory: 14056 KB -BaekjoonHub

---
+++ 백준/Bronze/11006. 남욱이의 닭장/남욱이의 닭장.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            // 2 * t + 1 * u = N;
+            // t + u = M;
+            // t + M = N;
+            // t = N - M;
+            int t = N - M;
+            int u = M - t;
+            sb.append(u).append(' ').append(t).append('\n');
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
