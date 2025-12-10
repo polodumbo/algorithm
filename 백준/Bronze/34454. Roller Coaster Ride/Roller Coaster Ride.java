cbf90669b176d99062bcc19704fc40f28a2ccfda
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2025-12-10 15:06:34 +0900

    [Bronze V] Title: Roller Coaster Ride, Time: 104 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/34454. Roller Coaster Ride/Roller Coaster Ride.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+        int P = Integer.parseInt(br.readLine());
+
+        if (C * P >= N) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+        br.close();
+    }
+}
