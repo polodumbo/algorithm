324dc23cc8e370b92fe3a719b33bd2f793d4d9b7
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2025-12-30 22:08:05 +0900

    [Bronze V] Title: 초코바, Time: 100 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()); // 100원 개수
+        int M = Integer.parseInt(st.nextToken()); // 초코바 가격
+
+        if (N * 100 < M) {
+            System.out.println("No");
+        } else {
+            System.out.println("Yes");
+        } 
+
+        br.close();
+    }
+}
