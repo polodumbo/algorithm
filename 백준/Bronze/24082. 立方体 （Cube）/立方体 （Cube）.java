83700e5cc1f0739650adb19acba06f8ed3bfe684
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2026-01-11 17:55:05 +0900

    [Bronze V] Title: 立方体 (Cube), Time: 104 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/24082. 立方体 （Cube）/立方体 （Cube）.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int X = Integer.parseInt(br.readLine());
+        int result = X * X * X;
+
+        System.out.println(result);
+        br.close();
+    }
+}
