1e0dd49532f2e896cfb244749e6f7cfd3f2551df
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2026-01-12 16:55:11 +0900

    [Bronze V] Title: Copier, Time: 104 ms, Memory: 13980 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            sb.append(num)
+              .append(' ')
+              .append(num)
+              .append('\n');
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
