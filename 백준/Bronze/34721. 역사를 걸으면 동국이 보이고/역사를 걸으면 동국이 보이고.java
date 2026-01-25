afa56dd83e405c37683bca78eb3cf0b4aebcb18a
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2026-01-25 23:21:40 +0900

    [Bronze V] Title: 역사를 걸으면 동국이 보이고, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/34721. 역사를 걸으면 동국이 보이고/역사를 걸으면 동국이 보이고.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            sb.append("I love DGU").append('\n');
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
