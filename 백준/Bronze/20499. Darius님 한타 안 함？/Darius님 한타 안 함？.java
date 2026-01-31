6ef0f06338a6b03d61ba8f910fad668c5cc32a95
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2026-01-31 23:07:56 +0900

    [Bronze IV] Title: Darius님 한타 안 함?, Time: 100 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/20499. Darius님 한타 안 함？/Darius님 한타 안 함？.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), "/");
+        int K = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+        int A = Integer.parseInt(st.nextToken());
+
+        if (D == 0 || K + A < D) {
+            System.out.println("hasu");
+        } else {
+            System.out.println("gosu");
+        }
+        br.close();
+    }
+}
