67fe0668181fcbcef1c87f4996b5413963b2a72b
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2026-02-01 11:52:37 +0900

    [Bronze IV] Title: Quality-Adjusted Life-Year, Time: 112 ms, Memory: 14632 KB -BaekjoonHub

---
+++ 백준/Bronze/22279. Quality－Adjusted Life－Year/Quality－Adjusted Life－Year.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        double QALY = 0.0;
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            double q = Double.parseDouble(st.nextToken());
+            double y = Double.parseDouble(st.nextToken());
+
+            QALY += q * y;
+        }
+
+        System.out.println(String.format("%.3f", QALY));
+        br.close();
+    }
+}
