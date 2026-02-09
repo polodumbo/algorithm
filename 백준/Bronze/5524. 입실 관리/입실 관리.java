aaeef4534cd09d0ced927c0907660fd11287068b
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2026-02-09 21:06:32 +0900

    [Bronze IV] Title: 입실 관리, Time: 100 ms, Memory: 14104 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5524. 입실 관리/입실 관리.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            String mixed = br.readLine();
+            sb.append(mixed.toLowerCase()).append('\n');
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
