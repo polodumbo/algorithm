3bcfefc460980dc879f416cfddc5b64bc14443e5
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2026-02-13 12:31:34 +0900

    [Bronze IV] Title: 모음의 개수, Time: 108 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+        br.close();
+    }
+}
