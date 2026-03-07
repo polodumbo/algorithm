6c0324045c53a7d968973d73163306d71cecc06a
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2026-03-07 15:17:14 +0900

    [Bronze V] Title: 라면 공식, Time: 100 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/30007. 라면 공식/라면 공식.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+
+            sb.append(a * (x - 1) + b).append('\n');
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
