f89e163e1341360baafa832f002fe842d4b5b3b8
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2026-03-13 15:51:45 +0900

    [Bronze IV] Title: 10부제, Time: 116 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/10797. 10부제/10부제.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int count = 0;
+
+        while (st.hasMoreElements()) {
+            int date = Integer.parseInt(st.nextToken());
+            if (date == num) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+        br.close();
+    }
+}
