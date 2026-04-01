8d0518a7df1e3ed644b53f6ebfbb60604f2b957a
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2026-04-01 09:32:44 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.05 ms, Memory: 84.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder(my_string);
+        
+        sb.replace(s, overwrite_string.length() + s,overwrite_string);
+        
+        return sb.toString();
+    }
+}
