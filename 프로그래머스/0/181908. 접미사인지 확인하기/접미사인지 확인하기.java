675c03ac0822ef6aa56355cc56f2e6751188b39e
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2026-04-24 01:50:24 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.03 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String my_string, String is_suffix) {        
+        for (int i = 0; i < my_string.length(); i++) {
+            if (is_suffix.equals(my_string.substring(i))) {
+                return 1;
+            }
+        }
+        
+        return 0;
+    }
+}
