36617bf2ee680aba32e2c142c7b156e103de6c71
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2026-04-27 15:17:56 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.07 ms, Memory: 83.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int k) {        
+        int[] answer = new int[n / k];
+        
+        int idx = 0;
+        for (int i = k; i <= n; i += k) {
+            answer[idx++] = i;
+        }
+
+        return answer;
+    }
+}
