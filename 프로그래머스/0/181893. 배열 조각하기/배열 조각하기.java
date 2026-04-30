7e9b6f66574f212e38f2e274fdc2d6ed0da604c1
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2026-04-30 15:27:30 +0900

    [level 0] Title: 배열 조각하기, Time: 0.04 ms, Memory: 93.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int[] query) {
+        int start = 0;
+        int end = arr.length - 1;
+        
+        for (int i = 0; i < query.length; i++) {
+            if (i % 2 == 0) {
+                end = start + query[i];
+            } else {
+                start += query[i];
+            }
+        }
+        
+        int[] answer = Arrays.copyOfRange(arr, start, end + 1);
+        return answer;
+    }
+}
