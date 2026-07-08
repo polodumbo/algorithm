f2821ab7700d749a204a2bec986285e33460acd4
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2026-07-08 15:09:11 +0900

    [level 0] Title: 외계어 사전, Time: 0.03 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(String[] spell, String[] dic) {
+        for (int i = 0; i < dic.length; i++) {
+            int count = 0;
+            
+            for (int j = 0; j < spell.length; j++) {
+                if (!dic[i].contains(spell[j])) {
+                    break;
+                }
+                
+                count++;
+            }
+            
+            if (spell.length == count) {
+                return 1;
+            }
+        }
+        
+        return 2;
+    }
+}
