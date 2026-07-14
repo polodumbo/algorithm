1e4c95b6986a8da31f8f2cac31b8a053e2cb1b27
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2026-07-14 14:44:11 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.02 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int a, int b) {
+        int c = b / getGCD(a, b);
+        
+        while (c % 2 == 0) {
+                c /= 2;
+        }
+        
+        while (c % 5 == 0) {
+                c /= 5;
+        }
+        
+        return c == 1 ? 1 : 2;
+    }
+    
+    public int getGCD(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+	
+	return getGCD(b, a % b);
+}
+}
