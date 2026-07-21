e24f21342990a3a120a50541c32b42daf2b11f4a
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2026-07-21 13:05:51 +0900

    [level 0] Title: k의 개수, Time: 3.28 ms, Memory: 87.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int i, int j, int k) {
+        int answer = 0;
+        
+        for (int n = i; n <= j; n++) {
+            int num = n;
+            
+            while (num > 0) {
+                if (num % 10 == k) {
+                    answer++;
+                }
+                
+                num /= 10;
+            }
+        }
+        
+        return answer;
+    }
+}
