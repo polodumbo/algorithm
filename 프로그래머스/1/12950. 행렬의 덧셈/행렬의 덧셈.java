db57064126720244df535673c60f87d91db2ec5f
SungHoon Lee <104343387+polodumbo@users.noreply.github.com>
2026-09-01 02:05:21 +0900

    [level 1] Title: 행렬의 덧셈, Time: 2.38 ms, Memory: 127 MB -BaekjoonHub

---
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][];
+        
+        for (int i = 0; i < arr1.length; i++) {
+            answer[i] = new int[arr1[i].length];
+            
+            for (int j = 0; j < arr1[i].length; j++) {
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+        
+        return answer;
+    }
+}
